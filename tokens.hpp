c7da279be4662ba0a47598b8237ce03fdf5f5d26
thiagocalvi <thiagohcalvi@gmail.com>
2025-09-08 13:08:33 -0300

    Adicionando novos tokens da linguagem

--- tokens.hpp
+++ tokens.hpp
@@ -2,10 +2,16 @@
 #define TOKENS_HPP
 
 enum TokenType {
-		// Palavras reservadas
-		TOKEN_PROGRAM,
+		// Palavras reservadas da linguagem
+		TOKEN_PROGRAM = 256,
 		TOKEN_VAR,
 		TOKEN_INTEGER,
+		TOKEN_BOOLEAN,
+		TOKEN_PROCEDURE,
+		TOKEN_FUNCTION,
+		TOKEN_BEGIN,
+		TOKEN_END,
+		TOKEN_IF,
 };
 
 #endif
